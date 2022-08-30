7259293eac9212b58fbe27f2f5864aa368e88e9e
Evelyn Valles <evelynvalles7@gmail.com>
2022-08-30 18:27:14 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">Hello Dojo!</h1>
+      <p className="header">Things I need to do:</p>
+      <ul className="header">
+        <li>Learn React</li>
+        <li>Climb Mt.Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
